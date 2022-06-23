a66cfcff479abcfa60d64bd66a1687c7182a35e8
akswnd98 <akswnd98@gmail.com>
2022-06-24 00:47:19 +0900

    mobile layout 수정

--- src/App/Body/AboutMe/index.tsx
+++ src/App/Body/AboutMe/index.tsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import styles from './index.module.scss';
-import Row from './Row';
 import person from '@/assets/images/person-pictogram.svg';
 import call from '@/assets/images/call-pictogram.svg';
 import calendar from '@/assets/images/calendar-pictogram.svg';
@@ -31,16 +30,16 @@ function AboutMe () {
             <Pictogram icon={calendar} label={'생년월일'} value={'98.09.15'} />
           </div>
           <div>
-            <Pictogram icon={person} label={'특징'} value={'합리적 경험주의자'} />
+            <Pictogram icon={star} label={'특징'} value={'합리적 경험주의자'} />
           </div>
           <div>
-            <Pictogram icon={person} label={'연락처'} value={'010-8933-4509'} />
+            <Pictogram icon={call} label={'연락처'} value={'010-8933-4509'} />
           </div>
           <div>
-            <Pictogram icon={person} label={'이메일'} value={'akswnd98@gmail.com'} />
+            <Pictogram icon={mail} label={'이메일'} value={'akswnd98@gmail.com'} />
           </div>
           <div>
-            <Pictogram icon={person} label={'github'} value={'https://github.com/akswnd98'} />
+            <Pictogram icon={github} label={'github'} value={'https://github.com/akswnd98'} />
           </div>
         </div>
       </div>
